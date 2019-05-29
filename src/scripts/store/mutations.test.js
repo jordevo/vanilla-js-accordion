5b3d2c5cf7345ca467a2587152da6539eeb85e02
jordi <jordi.cat@gmail.com>
2019-05-29 18:46:20 +0200

    Added coverage configuration to Jest and improved mutations.js test to achieve 100% code coverage

--- src/scripts/store/mutations.test.js
+++ src/scripts/store/mutations.test.js
@@ -1,15 +1,45 @@
 import mutations from './mutations.js';
 
 const mockState = {
-    openPanels: ['id1','id2'],
-    factsShown: 2,
-    openMultiplePanels: false
-}
+	openPanels: ['id1','id2'],
+	factsShown: 2,
+	openMultiplePanels: false
+};
 
-test('openPanel mutation adds panel to state', () => {
-	expect(mutations.openPanel(mockState,'id3').openPanels).toContain('id3');
-});
+const mockStateAllowOpen = {
+	openPanels: ['id1','id2'],
+	factsShown: 2,
+	openMultiplePanels: true
+};
 
-test('closePanel mutation removes panel from state', () => {
-	expect(mutations.closePanel(mockState,'id3').openPanels).not.toContain('id3');
+describe('mutations', () => {
+
+	describe('when openPanel is called', () => {
+		test('when multiple panels cannot be opened, empties state and adds panel to state', () => {
+			expect(mutations.openPanel(mockState,'id3').openPanels).toEqual(['id3']);
+		});
+		//mockState.openMultiplePanels = true;
+		test('when multiple panels can be opened, adds panel to state', () => {
+			expect(mutations.openPanel(mockStateAllowOpen,'id3').openPanels).toContain('id3');
+		});
+	});
+
+	test('closePanel removes panel from state', () => {
+		expect(mutations.closePanel(mockState,'id3').openPanels).not.toContain('id3');
+	});
+
+	test('shownNewFact increments the counter for random facts that the user has seen', () => {
+		expect(mutations.shownNewFact(mockState).factsShown).toBe(3);
+	});
+
+	test('toggleMultiplePanels inverts the given value of openMultiplePanels', () => {
+		expect(mutations.toggleMultiplePanels(mockState).openMultiplePanels).toBe(true);
+		expect(mutations.toggleMultiplePanels(mockStateAllowOpen).openMultiplePanels).toBe(false);
+	});
+
+	test('toggleMultiplePanels closes panels if switched to false', () => {
+		expect(mutations.toggleMultiplePanels(mockStateAllowOpen).openPanels).toEqual([]);
+	});
+
+
 });
